b7bfbbfe91246dadba3f027dbf797faf34796b4d
Marcel POUOMEGNE <editionhorizonpositif@gmail.com>
2026-05-10 22:24:51 +0200

    Correction erreur Jinja2 templates

--- app/front.py
+++ app/front.py
@@ -10,7 +10,7 @@ from .cart import get_cart
 from .paypal_client import create_paypal_order
 
 router = APIRouter()
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory="app/templates", auto_reload=False)
 
 # ---------- Page boutique ----------
 @router.get("/shop")
