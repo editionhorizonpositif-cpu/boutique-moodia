92a66d1ed1ec53dce0586276fc85a830700450ae
Marcel POUOMEGNE <editionhorizonpositif@gmail.com>
2026-05-10 20:33:05 +0200

    Use base64 encoded Google credentials

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
 # app/config.py
 import os
+import json
+import base64
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,4 +12,11 @@ PAYPAL_CLIENT_SECRET = os.getenv("PAYPAL_CLIENT_SECRET")
 PAYPAL_MODE = os.getenv("PAYPAL_MODE", "sandbox")
 MOODIA_JWT_SECRET = os.getenv("MOODIA_JWT_SECRET")
 APP_SECRET_KEY = os.getenv("APP_SECRET_KEY")
-GOOGLE_DRIVE_CREDENTIALS = os.getenv("GOOGLE_DRIVE_CREDENTIALS")
+GOOGLE_DRIVE_CREDENTIALS = os.getenv("GOOGLE_DRIVE_CREDENTIALS")  # ancienne variable (chemin)
+
+# Nouvelle variable : chaîne base64 des credentials Google
+_creds_b64 = os.getenv("GOOGLE_DRIVE_CREDENTIALS_BASE64")
+if _creds_b64:
+    GOOGLE_DRIVE_CREDENTIALS_DICT = json.loads(base64.b64decode(_creds_b64))
+else:
+    GOOGLE_DRIVE_CREDENTIALS_DICT = None
