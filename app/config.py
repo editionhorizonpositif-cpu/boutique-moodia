1b3c75cd99ad83419aa82d5c83903ec138a86b1d
MARCEL POUOMEGNE <editionhorizonpositif@gmail.com>
2026-05-11 00:23:55 +0200

    Add SendGrid configuration to app config

--- app/config.py
+++ app/config.py
@@ -21,4 +21,7 @@ else:
     GOOGLE_DRIVE_CREDENTIALS_DICT = None
 
 RETURN_URL = os.getenv("RETURN_URL", "http://127.0.0.1:8000/payment-success")
-CANCEL_URL = os.getenv("CANCEL_URL", "http://127.0.0.1:8000/payment-cancel")
+CANCEL_URL = os.getenv("CANCEL_URL", "http://127.0.0.1:8000/payment-cancel") 
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+SENDGRID_FROM_EMAIL = os.getenv("SENDGRID_FROM_EMAIL")
+SENDGRID_FROM_NAME = os.getenv("SENDGRID_FROM_NAME", "Moodia Support")
